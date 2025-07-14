6cca113af17f5b0be75ae5484e4d568f1dddd1d9
cevher ozden <efeozden@gmail.com>
2025-07-14 23:22:09 +0300

    Home Page React

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+function Main() {
+  return (
+<>
+<div className='container'>
+    <div className='p-5 text-center bg-light-dark rounded'>
+        <h1 className='text-light'>Stock Prediction Portal to the Stock Prediction Portal</h1>
+        <p className='text-light lead'>This is a simple stock prediction portal built with React and Django Rest Framework.</p>
+        <Button text='Get Started' class='btn-info' /    >
+    </div>
+</div>
+</>  
+)
+}
+
+export default Main
